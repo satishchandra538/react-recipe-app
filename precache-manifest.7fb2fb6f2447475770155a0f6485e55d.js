467bdd039809d212ef52ff038d771b9c042d2ad6
satishchanda538 <Satishchandra538@gmail.com>
2020-02-02 17:14:53 +0530

    Updates

---
+++ precache-manifest.7fb2fb6f2447475770155a0f6485e55d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7aabbc8790b9c6efb7f98cb1d42ea8",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "ea98289bbdb118376165",
+    "url": "/react-recipe-app/static/css/main.f44e673a.chunk.css"
+  },
+  {
+    "revision": "588d642ee31efbe27edd",
+    "url": "/react-recipe-app/static/js/2.bbbc862e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-recipe-app/static/js/2.bbbc862e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea98289bbdb118376165",
+    "url": "/react-recipe-app/static/js/main.5c1f3130.chunk.js"
+  },
+  {
+    "revision": "63b450cb2ec5759458c8",
+    "url": "/react-recipe-app/static/js/runtime-main.3772d7a9.js"
+  }
+]);
